cf8d22acbce8bbea9999f591f7fcb9d3b3b44f5b
prathvitomar <108338096+prathvitomar@users.noreply.github.com>
2023-11-06 18:54:09 +0530

    Created Authentication Service for User Login

--- mega-app/src/App.jsx
+++ mega-app/src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL)
+  // console.log(import.meta.env.VITE_APPWRITE_URL)
   return (
     <>
   <h1>This is Mega App</h1>
