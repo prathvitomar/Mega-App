5ec9edc021c05009a2bdeda5e86c3a48d31ee5d1
prathvitomar <108338096+prathvitomar@users.noreply.github.com>
2024-04-09 20:01:04 +0530

    Created Login,SignUp and Authentication Layout

--- mega-app/src/components/index.js
+++ mega-app/src/components/index.js
@@ -3,11 +3,21 @@ import Footer from "./Footer/Footer";
 import Container  from "./Container/Container";
 import Logo from "./Logo"
 import LogoutBtn from "./LogoutBtn";
+import Select from "./Select";
+import AuthLayout from "./AuthLayout";
+import PostCard from "./PostCard";
+import Login from "./Login";
+import Signup from "./Signup";
 
 export {
     Header,
     Footer,
     Container,
     Logo,
-    LogoutBtn
+    LogoutBtn,
+    PostCard,
+    Login,
+    Signup,
+    AuthLayout,
+    Select
 }
