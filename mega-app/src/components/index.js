2e813e66fa0c1aedc83e766f487fff2872efab8f
prathvitomar <108338096+prathvitomar@users.noreply.github.com>
2024-04-10 18:21:14 +0530

    Created Pages and Routes

--- mega-app/src/components/index.js
+++ mega-app/src/components/index.js
@@ -1,27 +1,30 @@
+import Select from "./Select";
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
-import Container  from "./Container/Container";
-import Logo from "./Logo"
-import Button from "./Button"
-import Input from "./Input"
-import LogoutBtn from "./LogoutBtn";
-import Select from "./Select";
-import AuthLayout from "./AuthLayout";
-import PostCard from "./PostCard";
-import Login from "./Login";
+import Container from "./container/Container";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+import RTE from "./RTE";
 import Signup from "./Signup";
-
+import Login from "./Login";
+import Button from "./Button";
+import PostForm from "./post-form/PostForm";
+import PostCard from "./PostCard";
+import AuthLayout from "./AuthLayout";
+import Input from "./Input";
 export {
     Header,
     Footer,
     Container,
-    Button,
-    Input,
     Logo,
     LogoutBtn,
-    PostCard,
-    Login,
+    RTE,
     Signup,
-    AuthLayout,
-    Select
+    Login,
+    Button,
+    PostForm,
+    PostCard,
+    AuthLayout, 
+    Input,
+    Select,
 }
